07862c1057ec95b60f7bedb6cfddacb03ccfda8c
xjh0803 <1259007219@qq.com>
2025-02-13 00:19:00 +0800

    Set lr to 2e5

--- finetune.sh
+++ finetune.sh
@@ -9,7 +9,7 @@ export NCCL_NVLS_ENABLE=0
 export VISION_ENCODER_NAME="/mnt/wangxiaofa/RDT_module_params/rdt_param/siglip-so400m-patch14-384"
 export TEXT_ENCODER_NAME="/mnt/wangxiaofa/RDT_module_params/rdt_param/t5-v1_1-xxl"
 # export OUTPUT_DIR="/mnt/wangxiaofa/rdt_checkpoint/170M_ac8_bs32x8_last_padding/"
-export OUTPUT_DIR="/mnt/wangxiaofa/rdt_checkpoint/0212_1000M_ac12_bs6x16_last_pizza/"
+export OUTPUT_DIR="/mnt/wangxiaofa/rdt_checkpoint/0212_1000M_ac12_lr2e5_bs6x16_last_pizza/"
 export PRETRAIN_MODEL_PATH="/mnt/wangxiaofa/RDT_module_params/rdt_param/rdt-1b/"
 
 # export TEXT_ENCODER_NAME="/datahdd_8T/vla_pizza/RDT_module_params/t5-v1_1-xxl/"
@@ -49,7 +49,7 @@ deepspeed --hostfile=hostfile.txt main.py \
     --sample_period=500 \
     --checkpoints_total_limit=100 \
     --lr_scheduler="constant" \
-    --learning_rate=1e-4 \
+    --learning_rate=2e-5 \
     --mixed_precision="bf16" \
     --dataloader_num_workers=8 \
     --image_aug \
